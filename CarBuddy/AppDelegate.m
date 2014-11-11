f468101bf763463ad52ff6e37b335d93e6dcb02a
Vitaly Chupryk <vitaly@softmaxsg.com>
2014-11-11 21:03:29 +0200

    Started CarBuddy UI

--- CarBuddy/AppDelegate.m
+++ CarBuddy/AppDelegate.m
@@ -11,7 +11,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [self configureNavigationBarsAppearance];
+
     return YES;
+}
+
+- (void)configureNavigationBarsAppearance
+{
+    UINavigationBar *appearance = [UINavigationBar appearance];
+
+    appearance.titleTextAttributes = @{ NSFontAttributeName: [UIFont fontWithName:@"AvenirNextCondensed-DemiBold" size:22] };
 }
 
 @end
--- CarBuddy/UI/Views & Controllers/MainViewController.m
+++ CarBuddy/UI/Views & Controllers/MainViewController.m
@@ -7,6 +7,27 @@
 
 #import "MainViewController.h"
 
+@interface MainViewController ()
+
+@property (nonatomic) IBOutlet UIImageView *carImageView;
+@property (nonatomic) IBOutlet UIImageView *walletImageView;
+
+- (IBAction)closeModalSegue:(UIStoryboardSegue *)segue;
+
+@end
+
 @implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.carImageView.image = [self.carImageView.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    self.walletImageView.image = [self.walletImageView.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+}
+
+- (IBAction)closeModalSegue:(UIStoryboardSegue *)segue
+{
+}
 
 @end
